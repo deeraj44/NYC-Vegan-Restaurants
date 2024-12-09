d66b5ca58690f104e202065c5d70d865232e6663
MDhopate <131405501+MDhopate@users.noreply.github.com>
2024-12-09 01:44:34 -0500

    Update popup.js - message box

--- assets/js/popup.js
+++ assets/js/popup.js
@@ -2,15 +2,21 @@
 const popup = document.getElementById("data-source-popup");
 const openButton = document.getElementById("data-source-button");
 const closeButton = document.getElementById("close-popup");
+const okButton = document.getElementById("ok-button");
 
 // Open the pop-up
 openButton.addEventListener("click", () => {
-    popup.style.display = "flex";
+    popup.style.display = "flex"; // Show the modal
 });
 
 // Close the pop-up
 closeButton.addEventListener("click", () => {
-    popup.style.display = "none";
+    popup.style.display = "none"; // Hide the modal
+});
+
+// Close pop-up when "OK" button is clicked
+okButton.addEventListener("click", () => {
+    popup.style.display = "none"; // Hide the modal
 });
 
 // Close pop-up when clicking outside of it
